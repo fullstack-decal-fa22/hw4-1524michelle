716ec793ad7a1afd751ce1213df5421b71ae8b82
Michelle Huang <1524michelle@berkeley.edu>
2022-10-24 23:50:03 -0700

    hw4

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Button from './Button';
 /* Add any imports you think you might need here! */
 
 const Menu = () => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          {<Button text="red" />}
+          {<Button text="yellow" />}
+          {<Button text="orange" />}
+          {<Button text="pink" />}
       </div>
     );
 }
